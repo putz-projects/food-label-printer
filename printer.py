2735918566e23032e079eea7678cf85531a86f13
Shreyas Kapur <shreyaskapur@yahoo.com>
2018-12-11 20:10:33 -0500

    Visual tweaks again.

--- printer.py
+++ printer.py
@@ -9,16 +9,16 @@ def print_label(kerberos, date):
     """
 
     # Initialize image and fonts.
-    img = Image.new('RGB', (500, 200), color='white')
+    img = Image.new('RGB', (500, 250), color='white')
     font_regular = ImageFont.truetype('Roboto-Regular.ttf', 46)
-    font_light = ImageFont.truetype('Roboto-Regular.ttf', 36)
-    font_italic = ImageFont.truetype('Roboto-Regular.ttf', 24)
+    font_light = ImageFont.truetype('Roboto-Regular.ttf', 46)
+    font_italic = ImageFont.truetype('Roboto-Regular.ttf', 36)
 
     # Draw the text.
     draw = ImageDraw.Draw(img)
     draw.text((30, 20), kerberos, font=font_regular, fill=(0, 0, 0))
     draw.text((30, 90), date.strftime("%m/%d/%Y").lower(), font=font_light, fill=(0, 0, 0))
-    draw.text((30, 140), date.strftime("%I:%M %p").lower(), font=font_italic, fill=(0, 0, 0))
+    draw.text((30, 160), date.strftime("%I:%M %p").lower(), font=font_italic, fill=(0, 0, 0))
 
     # Build a QR code.
     qr = qrcode.QRCode(box_size = 5, border=1)
@@ -31,7 +31,7 @@ def print_label(kerberos, date):
     img.save('label.png')
 
     # Print it!
-    subprocess.call(["lp", "-o", "fit-to-page", "-o", "orientation-requested=6", "label.png"])
+    # subprocess.call(["lp", "-o", "fit-to-page", "-o", "orientation-requested=6", "label.png"])
 
 if __name__ == '__main__':
     print_label("shreyask", datetime.datetime.now())
